9e1c2fcf83580722197374b7858a176367dae352
mcooperr <mcooperr@umich.edu>
2022-11-16 23:19:00 -0500

    final

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,15 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients (id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, age INTEGER, cuteness INTEGER, agressiveness INTEGER)")
+    conn.commit()
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("INSERT INTO Patients (id, name, species_id, age, cuteness, agressiveness)VALUES(?,?,?,?,?,?)") ('0', 'Fluffle', '0', '3', '90', '100')
+    conn.commit()
     
 
 # TASK 2
@@ -59,7 +62,12 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    count = 1
+    for data in json_data:
+        pet_id = count
+        count += 1
+        name = data['name']
+        cur.execute('SELECT id FROM Species WHERE title = ?', (data['species'],))
 
 
 # TASK 3
